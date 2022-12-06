40e7a0dbd4df5d48a81acb5b2cba28f6195dfc2e
Myriam D <diabagatemyriam@gmail.com>
2022-12-06 14:45:32 -0500

    refractured redux, api, db

--- spillover-code/Redux/Shopping/shopping-types.js
+++
@@ -1,7 +0,0 @@
-export const ADD_TO_CART = 'ADD_TO_CART'
-export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
-export const ADD_ONE = 'ADD_ONE'
-export const ADJUST_QTY = 'ADJUST_QTY'
-export const REMOVE_ONE_FROM_CART = 'REMOVE_ONE_FROM_CART'
-export const LOAD_CURRENT_ITEM = 'LOAD_CURRENT_ITEM'
-export const SET_DATA = "SET_DATA"
--- spillover-code/Redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import shopReducer from './Shopping/shopping-reducer';
-
-const rootReducer = combineReducers({
-    shop: shopReducer
-});
-
-// export default rootReducer;
