f695131746978e6ca8950136953ffafdb6c983fa
Myriam D <diabagatemyriam@gmail.com>
2023-01-13 20:37:06 -0500

    finished crud api

--- api/app.py
+++ api/app.py
@@ -1,15 +1,10 @@
 import firebase_admin
 import json
-
-# import pyrebase
-# import json
 import os
 
-# from fastapi import FastAPI
-# from fastapi.params import Body
-from pydantic import BaseModel
+
 from flask import Flask, request, render_template, redirect, flash, session, jsonify
-from flask_pydantic import validate
+from random import randrange
 from flask_cors import CORS
 from firebase_admin import credentials, firestore, initialize_app
 from flask_restful import Resource, Api, reqparse, abort
@@ -20,92 +15,71 @@ from typing import Optional
 cred = credentials.Certificate('./helpers/fbAdminConfig.json')
 default_app = initialize_app(cred)
 db = firestore.client()
-
 products = db.collection('products')
-
-
 
 
 app = Flask(__name__)
 api = Api(app)
-# app = FastAPI()
 CORS(app)
 
 
 app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 
-# users = [{'uid': 1, 'name': 'Md Schairer'}]
-# @app.route('/')
-# def userinfo():
-#     return {'data': users}, 200
+product_post_args = reqparse.RequestParser()
+product_post_args.add_argument("name", type=str, help="Add a name", required=True)
+product_post_args.add_argument("price", type=str, help="Add a price", required=True)
+product_post_args.add_argument("description", type=str, help="Add a description", required=True)
+product_post_args.add_argument("imageUrl", type=str, help="Add image", required=True)
+product_post_args.add_argument("id", type=int, help="Add id", required=False)
 
-names = {
-'Myriam': {'age': 24, 'gender': 'female'} , 
-'SM': {'age': 45, 'gender': 'male'}
-}
 
-# product_id = request.args.get('id')
+
+def find_product(id):
+    for p in all_products:
+        if p['id'] == id:
+            return p
+
+
 all_products = [doc.to_dict() for doc in products.stream()]
-# print(json.dumps(all_products))
-
-class QueryParams(BaseModel):
-    name: Optional[str]
-    # price: str
-    # description: str
-    # imageUrl: str
-
-class ResponseModel(BaseModel):
-  name: str
 
 
 
+class Products(Resource):
+  def get(self):
+      return jsonify({"results": all_products})
+  
+  def post(self):
+    args = product_post_args.parse_args()
+    args['id'] = randrange(0, 100000)
+    products.document(args['name']).set(args)
+    return {'sucess': args}
 
+class Product(Resource):
+  def get(self, product_id):
+    product = find_product(product_id)
 
+    return jsonify({"product": product})
 
-
-# class Products(Resource):
-  @app.route('/', methods=["GET"])
-  @validate()
-  def get():
-    # product_id = request.args.get('id')
-    # all_products = [doc.to_dict() for doc in products.stream()]
-    # print('hello')
-    # {'Hello': 'world'}, 200
-     
-    return jsonify({"results": all_products})
+  def put(self, product_id):
+    args = product_post_args.parse_args()
+    product = find_product(product_id)
+    args['id'] = product['id']
     
-  @app.route('/new', methods=["POST"])
-  @validate()
-  def post(query: QueryParams):
-    print(query)
-    return {'sucess': f'{query}'}
-    # ResponseModel(
-    #   name=name
-    # )
+    products.document(product['name']).set(args, merge=True)
+    return{'success': args}
 
-  def put():
-    pass
+  def delete(self, product_id):
+    product = find_product(product_id)
+    products.document(product['name']).delete()
+    return {'message': "delete"}
 
-# class User(Resource):
-#     def get(self, user_id):
-#         abort_if_user_id_doesnt_exist(user_id)
-#         return users[user_id]
+    
 
-#     def put(self, user_id):
-#         abort_if_user_already_exists(user_id)
-#         args = user_put_args.parse_args()       
-#         users[user_id] = args
-#         return users[user_id], 201 
 
-#     def delete(self, user_id):
-#         abort_if_user_id_doesnt_exist(user_id)
-#         del users[user_id]
-#         return '', 204
-
-# api.add_resource(HelloWorld, '/<string:name>')
+api.add_resource(Products, '/')
+api.add_resource(Product, "/<int:product_id>")
 # api.add_resource(Products, "/")
-# api.add_resource(Products, "/new")
 
 if __name__ == '__main__':
     app.run(debug=True)
